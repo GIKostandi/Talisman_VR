13402afd4a0de76186c2428a8fc9d752710bf6ca
George <kostandi.gosha@yandex.ru>
2024-11-28 16:19:56 +0300

    Много всего нового

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ var createScene = function () {
   let wrapper = new BABYLON.TransformNode("Wrapper");
   wrapper.setEnabled(true);
   wrapper.accessibilityTag = {
-      description: "Scene"
+    description: "Scene",
   };
 
   // Поле для телепортации
@@ -76,53 +76,154 @@ var createScene = function () {
   vrHelper.enableTeleportation({ floorMeshes: [ground] });
 
   const allObjects = [
-    ...jsonData.persons.map((person) => ({
-      model: "./models/person.obj",
-      name: person.name,
-      type: "person",
-    })),
+    //если роль - ученый
+    ...jsonData.persons
+      .filter((person) => person.role === "Ученый")
+      .map((person) => ({
+        model: "./models/science.obj",
+        name: person.name,
+        type: person.type,
+        role: person.role,
+        url: person.url,
+      })),
+    //Если роль не ученый
+    ...jsonData.persons
+      .filter((person) => person.role != "Ученый")
+      .map((person) => ({
+        model: "./models/person.obj",
+        name: person.name,
+        type: person.type,
+        role: person.role,
+        url: person.url,
+      })),
+
+    // Организации
     ...jsonData.organizations.map((organization) => ({
       model: "./models/organization.obj",
       name: organization.name,
-      type: "organization",
+      type: organization.type,
+      role: organization.role,
+      url: organization.url,
     })),
+
+    // Сообщества
     ...jsonData.communities.map((community) => ({
       model: "./models/map.obj",
       name: community.name,
-      type: "community",
+      type: community.type,
+      role: community.role,
+      url: community.url,
     })),
   ];
+  // Функция для перемешивания массива
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
+  shuffleArray(allObjects);
+
   console.log(allObjects);
 
   // GUI для текста
   const advancedTexture =
     BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
 
-  // Функции для объектов и текстов
-  const createLabel = (name, position) => {
-    const plane = BABYLON.MeshBuilder.CreatePlane(
-      "labelPlane",
-      { size: 1 },
-      scene
-    );
-    plane.parent=wrapper;
-    plane.position = position.clone();
-    plane.position.y += 1.1;
+  //Отрисовка карточки над концептом
+  const createLabel = async (name, position) => {
+    try {
+      const plane = BABYLON.MeshBuilder.CreatePlane(
+        "labelPlane",
+        { width: 1, height: 0.6 },
+        scene
+      );
+      plane.parent = wrapper;
+      plane.position = position.clone();
+      plane.position.y += 1.2;
+      plane.position.z -= 0.2;
+      plane.rotation = new BABYLON.Vector3(0, Math.PI, 0);
 
-    plane.rotation = new BABYLON.Vector3(0, Math.PI, 0);
-    const dynamicTexture =
-      BABYLON.GUI.AdvancedDynamicTexture.CreateForMesh(plane);
-    dynamicTexture.parent=wrapper;
+      const material = new BABYLON.StandardMaterial("labelMaterial", scene);
 
-    const textBlock = new BABYLON.GUI.TextBlock();
-    textBlock.text = name;
-    textBlock.color = "black";
-    textBlock.fontSize = 100;
-    dynamicTexture.addControl(textBlock);
+      const textureWidth = 512;
+      const textureHeight = 256;
+      const dynamicTexture = new BABYLON.DynamicTexture(
+        "dynamicTexture",
+        { width: textureWidth, height: textureHeight },
+        scene,
+        true
+      );
 
-    return plane;
+      material.diffuseColor = new BABYLON.Color3(255, 255, 255);
+
+      plane.material = material;
+      material.diffuseTexture = dynamicTexture;
+      material.backFaceCulling = false;
+      plane.material = material;
+      plane.background = "white";
+
+      // Отрисовка текста
+      const ctx = dynamicTexture.getContext();
+      ctx.clearRect(0, 0, textureWidth, textureHeight);
+      ctx.fillStyle = "white";
+      ctx.fillRect(0, 0, textureWidth, textureHeight);
+
+      ctx.font = "bold 45px Arial";
+      ctx.fillStyle = "rgb(0,68,255)";
+      ctx.textAlign = "center";
+      ctx.textBaseline = "middle";
+
+      const maxWidth = textureWidth - 20;
+      const lineHeight = 55;
+      let lines = [];
+      let currentLine = "";
+
+      // Разбиение текста на строки
+      name.split(" ").forEach((word) => {
+        const testLine = currentLine ? currentLine + " " + word : word;
+        const width = ctx.measureText(testLine).width;
+
+        if (width < maxWidth) {
+          currentLine = testLine;
+        } else {
+          lines.push(currentLine);
+          currentLine = word;
+        }
+      });
+
+      if (currentLine) {
+        lines.push(currentLine);
+      }
+
+      // Отрисовка текста
+      const totalHeight = lines.length * lineHeight;
+      const yStart = (textureHeight - totalHeight) / 1.5;
+
+      // Рисуем все строки текста
+      lines.forEach((line, index) => {
+        ctx.fillText(line, textureWidth / 2, yStart + index * lineHeight);
+      });
+
+      dynamicTexture.update();
+
+      return plane;
+    } catch (error) {
+      console.error("Ошибка создания метки:", error);
+      return null;
+    }
   };
-  const createObject = async (modelName, name, positionX, positionY) => {
+
+  const createObject = async (
+    modelName,
+    name,
+    positionX,
+    positionY,
+    type,
+    role,
+    url
+  ) => {
     try {
       // Загрузка модели
       const result = await BABYLON.SceneLoader.ImportMeshAsync(
@@ -133,7 +234,7 @@ var createScene = function () {
       );
       if (result.meshes.length > 0) {
         const mesh = result.meshes[0];
-        mesh.parent=wrapper;
+        mesh.parent = wrapper;
         mesh.position = new BABYLON.Vector3(positionX, positionY, 0);
 
         createLabel(name, mesh.position);
@@ -143,14 +244,21 @@ var createScene = function () {
 
         mesh.actionManager = new BABYLON.ActionManager(scene);
 
-        //Отображение карточки концепта при нажатии
+        // Создание карточки
+        const card = createConceptCard(name, type, role, url);
+
+        // Отображение карточки концепта при нажатии
         mesh.actionManager.registerAction(
-          new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger, () => {
+          new BABYLON.ExecuteCodeAction(
+            BABYLON.ActionManager.OnPickTrigger,
+            () => {
               wrapper.setEnabled(false);
               card.setEnabled(true);
-          })
-      );
-        //Поднитие при наведении
+            }
+          )
+        );
+
+        // Поднятие при наведении
         mesh.actionManager.registerAction(
           new BABYLON.ExecuteCodeAction(
             BABYLON.ActionManager.OnPointerOverTrigger,
@@ -160,7 +268,7 @@ var createScene = function () {
           )
         );
 
-        //вернуть обратно
+        // Вернуть обратно
         mesh.actionManager.registerAction(
           new BABYLON.ExecuteCodeAction(
             BABYLON.ActionManager.OnPointerOutTrigger,
@@ -179,23 +287,23 @@ var createScene = function () {
 
   // Обработка данных JSON
   let positionX = -2.5;
-  let positionY = 0;
+  let positionY = -2.4;
   const maxItemsPerShelf = 5;
   let itemsOnCurrentShelf = 0;
 
+  //отрисовка полки
   const createShelf = (positionY) => {
     const shelf = BABYLON.MeshBuilder.CreateBox(
       "shelf",
       { height: 0.05, width: 6, depth: 0.5 },
       scene
     );
-    shelf.parent=wrapper;
+    shelf.parent = wrapper;
     shelf.position.y = positionY;
 
     const shelfMaterial = new BABYLON.StandardMaterial("shelfMaterial", scene);
-    shelfMaterial.diffuseColor = new BABYLON.Color3.Blue();
+    shelfMaterial.diffuseColor = new BABYLON.Color3(0, 68 / 255, 255 / 255);
 
-    // Применение материала к полке
     shelf.material = shelfMaterial;
     return shelf;
   };
@@ -205,130 +313,120 @@ var createScene = function () {
   (async () => {
     for (const object of allObjects) {
       if (itemsOnCurrentShelf >= maxItemsPerShelf) {
-        positionY += 1.3;
+        positionY += 1.8;
         createShelf(positionY);
         positionX = -2.5;
         itemsOnCurrentShelf = 0;
       }
 
-      await createObject(object.model, object.name, positionX, positionY);
+      await createObject(
+        object.model,
+        object.name,
+        positionX,
+        positionY,
+        object.type,
+        object.role,
+        object.url
+      );
       positionX += 1.2;
       itemsOnCurrentShelf += 1;
     }
   })();
 
-  //Отрисовка карточки с данными о концепте (засунуть в функцию и вынести в отдельный файл)
-  let card = BABYLON.MeshBuilder.CreatePlane("card", { size: 5 });
-  card.rotation = new BABYLON.Vector3(0, Math.PI, 0);
-  card.setEnabled(false);
-  let adt = BABYLON.GUI.AdvancedDynamicTexture.CreateForMesh(card);
+  const createConceptCard = (name, type, role, url) => {
+    let card = BABYLON.MeshBuilder.CreatePlane("card", { size: 5 });
+    card.rotation = new BABYLON.Vector3(0, Math.PI, 0);
+    card.setEnabled(false);
 
-  let wrapFront = new BABYLON.GUI.Rectangle("Card_wrapFront");
-  wrapFront.width = "100%";
-  wrapFront.background = "white";
-  adt.addControl(wrapFront);
+    let adt = BABYLON.GUI.AdvancedDynamicTexture.CreateForMesh(card);
 
-  let thumbnailBg = new BABYLON.GUI.Rectangle("Card_ThumbnailBg");
-  thumbnailBg.accessibilityTag = {
-      description: "Event card"
-  };
-  thumbnailBg.width = "100%";
-  thumbnailBg.height = "40%";
-  thumbnailBg.background = "white";
-  thumbnailBg.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
-  thumbnailBg.top = 10;
-  thumbnailBg.right = 100;
-  wrapFront.addControl(thumbnailBg);
+    let wrapFront = new BABYLON.GUI.Rectangle("Card_wrapFront");
+    wrapFront.width = "100%";
+    wrapFront.background = "white";
+    adt.addControl(wrapFront);
 
-  let url = "https://upload.wikimedia.org/wikipedia/commons/8/80/Sofja_Wassiljewna_Kowalewskaja_1_%28Remini_enhanced%29.jpg";
-  let ConceptPhoto = new BABYLON.GUI.Image("Card_ThumbnailImage", url);
-  ConceptPhoto.width = "50%";
-  ConceptPhoto.height = "100%";
-  ConceptPhoto.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
-  thumbnailBg.addControl(ConceptPhoto);
+    let thumbnailBg = new BABYLON.GUI.Rectangle("Card_ThumbnailBg");
+    thumbnailBg.accessibilityTag = {
+      description: "Event card",
+    };
+    thumbnailBg.width = "100%";
+    thumbnailBg.height = "40%";
+    thumbnailBg.background = "white";
+    thumbnailBg.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
+    thumbnailBg.top = 10;
+    thumbnailBg.right = 100;
+    wrapFront.addControl(thumbnailBg);
 
-  const ConceptName = new BABYLON.GUI.TextBlock("ConceptName", "Концепт: Софья Ковалевская");
-  ConceptName.fontSize = 33;
-  ConceptName.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
-  ConceptName.textHorizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
-  ConceptName.right = "5%"
-  ConceptName.top = "120px";
-  ConceptName.height = "5%";
-  ConceptName.paddingLeft = "550px";
-  wrapFront.addControl(ConceptName);
+    let ConceptPhoto = new BABYLON.GUI.Image("Card_ThumbnailImage", url);
+    ConceptPhoto.width = "50%";
+    ConceptPhoto.height = "100%";
+    ConceptPhoto.horizontalAlignment =
+      BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+    thumbnailBg.addControl(ConceptPhoto);
 
-  const ConceptType = new BABYLON.GUI.TextBlock("ConceptType", "Тип концепта: Персона");
-  ConceptType.fontSize = 33;
-  ConceptType.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
-  ConceptType.textHorizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
-  ConceptType.right = "5%";
-  ConceptType.top = "180px";
-  ConceptType.height = "5%";
-  ConceptType.paddingLeft = "550px";
-  wrapFront.addControl(ConceptType);
+    const ConceptName = new BABYLON.GUI.TextBlock(
+      "ConceptName",
+      `Концепт: ${name}`
+    );
+    ConceptName.fontSize = 33;
+    ConceptName.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
+    ConceptName.textHorizontalAlignment =
+      BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+    ConceptName.right = "5%";
+    ConceptName.top = "120px";
+    ConceptName.height = "5%";
+    ConceptName.paddingLeft = "550px";
+    wrapFront.addControl(ConceptName);
 
-  const ConceptRole = new BABYLON.GUI.TextBlock("Conceptrole", "Деятельность: Ученый");
-  ConceptRole.fontSize = 33;
-  ConceptRole.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
-  ConceptRole.textHorizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
-  ConceptRole.right = "5%";
-  ConceptRole.top = "240px";
-  ConceptRole.height = "5%";
-  ConceptRole.paddingLeft = "550px";
-  wrapFront.addControl(ConceptRole);
+    const ConceptType = new BABYLON.GUI.TextBlock(
+      "ConceptType",
+      `Тип концепта: ${type}`
+    );
+    ConceptType.fontSize = 33;
+    ConceptType.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
+    ConceptType.textHorizontalAlignment =
+      BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+    ConceptType.right = "5%";
+    ConceptType.top = "180px";
+    ConceptType.height = "5%";
+    ConceptType.paddingLeft = "550px";
+    wrapFront.addControl(ConceptType);
 
-  let dateFront = new BABYLON.GUI.TextBlock("Card_Date", "Every day");
-  wrapFront.addControl(dateFront);
-  dateFront.fontSize = 40;
-  dateFront.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
-  dateFront.textHorizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
-  dateFront.paddingLeft = "7.5%";
-  dateFront.top = "50%";
-  dateFront.height = "5%";
-  dateFront.isEnabled = false;
+    const ConceptRole = new BABYLON.GUI.TextBlock(
+      "ConceptRole",
+      `Деятельность: ${role}`
+    );
+    ConceptRole.fontSize = 33;
+    ConceptRole.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
+    ConceptRole.textHorizontalAlignment =
+      BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+    ConceptRole.right = "5%";
+    ConceptRole.top = "240px";
+    ConceptRole.height = "5%";
+    ConceptRole.paddingLeft = "550px";
+    wrapFront.addControl(ConceptRole);
 
-  const timeFront = new BABYLON.GUI.TextBlock("Card_Time", "00:00 - 23:59");
-  wrapFront.addControl(timeFront);
-  timeFront.fontSize = 40;
-  timeFront.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
-  timeFront.textHorizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
-  timeFront.paddingLeft = "35%";
-  timeFront.top = "50%";
-  timeFront.height = "5%";
-
-  const meetingDetail = new BABYLON.GUI.TextBlock(
-      "Card_GroupName",
-      "Help the little bunny rabbits get ready for Easter! Look at all the different colors to decorate Easter eggs with and pick out the shapes you'd like to wear in the parade. "
-  );
-  wrapFront.addControl(meetingDetail);
-  meetingDetail.fontSize = 40;
-  meetingDetail.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
-  meetingDetail.textHorizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
-  meetingDetail.paddingLeft = "7.5%";
-  meetingDetail.top = "55%";
-  meetingDetail.height = "30%";
-  meetingDetail.width = "100%";
-  meetingDetail.textWrapping = BABYLON.GUI.TextWrapping.WordWrapEllipsis;
-
-
-  let closeButton = BABYLON.GUI.Button.CreateSimpleButton("Card_Close", "X");
-  closeButton.accessibilityTag = {
+    let closeButton = BABYLON.GUI.Button.CreateSimpleButton("Card_Close", "X");
+    closeButton.accessibilityTag = {
       description: "close",
-  };
-  closeButton.background = "black";
-  closeButton.color = "white";
-  closeButton.fontSize = 40;
-  closeButton.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
-  closeButton.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_RIGHT;
-  closeButton.textBlock.textHorizontalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_CENTER;
-  closeButton.top = "0";
-  closeButton.height = "5%";
-  closeButton.width = "5%";
-  wrapFront.addControl(closeButton);
-  closeButton.onPointerClickObservable.add(() => {
+    };
+    closeButton.background = "black";
+    closeButton.color = "white";
+    closeButton.fontSize = 40;
+    closeButton.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
+    closeButton.horizontalAlignment =
+      BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_RIGHT;
+    closeButton.top = "0";
+    closeButton.height = "5%";
+    closeButton.width = "5%";
+    wrapFront.addControl(closeButton);
+    closeButton.onPointerClickObservable.add(() => {
       card.setEnabled(false);
       wrapper.setEnabled(true);
-  });
+    });
+
+    return card;
+  };
 
   return scene;
 };
